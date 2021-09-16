d9070cd24f9bdb76fd3da280d07ffd409b96aa89
Must_nu73 <Must_nu73@STHQ0118-14>
2021-09-16 14:18:31 +0400

    Add project files.

---
+++ WpfApp1/Abstract Factory/AbstractFactory.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1.Abstract_Factory
+{
+
+    public interface IChair
+    {
+        bool sitOn();
+        bool hasLegs();
+    }
+
+    public class ArtDecoChair : IChair
+    {
+        public bool hasLegs()
+        {
+            return false;
+        }
+
+        public bool sitOn()
+        {
+            return false;
+        }
+
+
+
+        public string ImagePath_ArtDecoChair { get; set; }
+    }
+
+
+
+    public class ModernChair : IChair
+    {
+        public bool hasLegs()
+        {
+            return true;
+        }
+
+        public bool sitOn()
+        {
+            return true;
+        }
+
+        public string ImagePath_ModernChair { get; set; }
+    }
+
+
+
+    public class VictorialChair : IChair
+    {
+        public bool hasLegs()
+        {
+            return true;
+        }
+
+        public bool sitOn()
+        {
+            return false;
+        }
+        public string ImagePath_VictorialChair { get; set; }
+    }
+
+
+
+
+    public interface ITable
+    {
+        bool sitOn();
+        bool hasLegs();
+    }
+
+
+    public class ArtDecoTable : ITable
+    {
+        public bool hasLegs()
+        {
+            return true;
+
+        }
+
+        public bool sitOn()
+        {
+            return false;
+        }
+    }
+
+
+    public class ModernTable : ITable
+    {
+        public bool hasLegs()
+        {
+            return false;
+        }
+
+        public bool sitOn()
+        {
+            return true;
+        }
+    }
+
+
+    public class VictorialTable : ITable
+    {
+        public bool hasLegs()
+        {
+            return false;
+        }
+
+        public bool sitOn()
+        {
+            return true;
+        }
+    }
+
+    public interface ISofa
+    {
+        bool CanEnlarge();
+
+    }
+    public class ArtDecoSofa : ISofa
+    {
+        public bool CanEnlarge()
+        {
+            return true;
+        }
+    }
+
+
+    public class ModernSofa : ISofa
+    {
+        public bool CanEnlarge()
+        {
+            return false;
+        }
+    }
+
+
+    public class VictorianSofa : ISofa
+    {
+        public bool CanEnlarge()
+        {
+            return true;
+        }
+    }
+
+
+
+    public interface IFurnitureFactory
+    {
+        IChair createChair();
+        ITable createTable();
+        ISofa createSofa();
+        void Show();
+    }
+
+
+
+
+    public class ModernFurnitureFactory : IFurnitureFactory
+    {
+        public IChair createChair()
+        {
+            return new ModernChair();
+        }
+
+        public ISofa createSofa()
+        {
+            return new ModernSofa();
+        }
+
+        public ITable createTable()
+        {
+            return new ModernTable();
+        }
+
+        public void Show()
+        {
+            MessageBox.Show("Art Deco Style");
+            var chair = createChair();
+            var table = createTable();
+            var sofa = createSofa();
+        }
+    }
+
+
+    public class VictorianFurnitureFactory : IFurnitureFactory
+    {
+        public IChair createChair()
+        {
+            return new VictorialChair();
+        }
+
+        public ISofa createSofa()
+        {
+            return new VictorianSofa();
+        }
+
+        public ITable createTable()
+        {
+            return new VictorialTable();
+        }
+
+        public void Show()
+        {
+            MessageBox.Show("Art Deco Style");
+            // Console.WriteLine("Victorian Style");
+            //var chair = createChair();
+            //var table = createTable();
+            //var sofa = createSofa();
+        }
+    }
+
+
+
+
+
+    public class ArtDecoFurnitureFactory : IFurnitureFactory
+    {
+        public IChair createChair()
+        {
+            return new ArtDecoChair();
+        }
+
+        public ISofa createSofa()
+        {
+            return new ArtDecoSofa();
+        }
+
+        public ITable createTable()
+        {
+            return new ArtDecoTable();
+        }
+
+        public void Show()
+        {
+            MessageBox.Show("Art Deco Style");
+            //Console.WriteLine("Art Deco Style");
+            var chair = createChair();
+            var table = createTable();
+            var sofa = createSofa();
+        }
+    }
+    public class AbstractFactory
+    {
+
+    }
+}
---
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Abstract_Factory;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IFurnitureFactory furnitureFactory { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = sender as ComboBox;
+            var s = item.SelectedIndex;
+
+            if (s == 0)
+            {
+                furnitureFactory = new ModernFurnitureFactory();
+                furnitureFactory.Show();
+            }
+            else if (s == 1)
+            {
+                furnitureFactory = new ArtDecoFurnitureFactory();
+                furnitureFactory.Show();
+            }
+            else if (s == 2)
+            {
+                furnitureFactory = new VictorianFurnitureFactory();
+                furnitureFactory.Show();
+            }
+        }
+    }
+}
